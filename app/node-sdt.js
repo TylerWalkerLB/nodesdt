835e4b4b91b982c1180339e13f7a7f00348b7a1b
Tyler Walker <tyler@lifeblue.com>
2016-11-13 20:06:50 -0600

    Removed `node-std.js` and moved that files to `app.js` for simplicity

--- app/node-sdt.js
+++
@@ -1,8 +0,0 @@
-const PhantomCalls = require('./phantom/phantomjs-calls');
-const Promise = require('promise');
-
-PhantomCalls.getPageTimings('http://www.chadtylerwalker.com', 1200, 800).then(result => {
-    let allPageTimings = JSON.parse(result);
-    console.log(allPageTimings[0].totalLoadTime);
-});
-
