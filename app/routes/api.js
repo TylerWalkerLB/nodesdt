699f3304e1c84ab06c0108905b43283eeb6907ba
Tyler Walker <tyler@lifeblue.com>
2016-12-09 10:26:11 -0600

    Added some sample calls to the "api.js" file

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,6 +1,7 @@
 ;(function(){
     // Dependencies
-    //const PhantomCalls = require('../util/phantom/phantomjs-controller');
+    const PhantomCalls = require('../util/phantom/phantomjs-controller');
+    const ElasticController = require('../util/elasticsearch/elastic-controller');
 
     //var app = require('koa');
     var router = require('koa-router')();
@@ -14,6 +15,18 @@
         .get('/embed', function*() {
             console.log('API is working');
             this.body = 'embed reached';
+        })
+
+        .get('/phantom', function*() {
+
+            ElasticController.clientInfo().then(response => {
+                console.log(response);
+            });
+
+            PhantomCalls.getPageLoadData('http://heifer.org/').then(response => {
+                console.log(response);
+            });
+
         });
 
     // Modules
