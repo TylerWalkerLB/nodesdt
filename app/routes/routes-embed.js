eb22d9c54af8c98af51e08fe0a438fef1dba343c
Tyler Walker <tyler@lifeblue.com>
2016-12-13 22:36:22 -0600

    Added some more query data to the tracking pixel request. Adjusted the embed code to build an array for future use with ElasticSearch

--- app/routes/routes-embed.js
+++ app/routes/routes-embed.js
@@ -3,6 +3,65 @@ import 'babel-polyfill';
 
 const embedRouter = koaRouter();
 
+/**
+ * Returns an array from the query parameters attached to a tracking pixel
+ *
+ * @param q
+ * @returns {Promise}
+ */
+let buildQueryArray = function(q) {
+
+    return new Promise((resolve, reject) => {
+
+        if (q === '' || q === null || q === {}) {
+            reject(new Error('Query object empty'));
+        }
+
+        /**
+         * Query Response data
+         */
+        let returnObj = {
+
+            location: {
+                fullURL: q.locationProtocol + '//' + q.locationHost + q.locationPathname,
+                protocol: q.locationProtocol,
+                host: q.locationHost,
+                pathname: q.locationPathname
+            },
+
+            timings: {
+                redirectStart: q.redirectStart,
+                redirectEnd: q.redirectEnd,
+                unloadEventStart: q.unloadEventStart,
+                unloadEventEnd: q.unloadEventEnd,
+                fetchStart: q.fetchStart,
+                domainLookupStart: q.domainLookupStart,
+                domainLookupEnd: q.domainLookupEnd,
+                connectStart: q.connectStart,
+                connectEnd: q.connectEnd,
+                requestStart: q.requestStart,
+                responseStart: q.responseStart,
+                responseEnd: q.responseEnd,
+                domLoading: q.domLoading,
+                domInteractive: q.domInteractive,
+                domContentLoadedEventStart: q.domContentLoadedEventStart,
+                domContentLoadedEventEnd: q.domContentLoadedEventEnd,
+                loadEventStart: q.loadEventStart,
+                loadEventEnd: q.loadEventEnd
+            },
+
+            navigator: {
+                oscpu: q.oscpu,
+                platform: q.platform,
+                userAgent: q.userAgent,
+                language: q.language
+            }
+        };
+
+        resolve(returnObj);
+    });
+};
+
 embedRouter
 
     .get('/embed', function*() {
@@ -12,7 +71,15 @@ embedRouter
     })
 
     .get('/embed/tracking/:file', function*() {
-        console.log(this.query);
+
+        let fullQuery = this.query;
+
+        buildQueryArray(fullQuery).then(response => {
+            console.log(response);
+        }).catch(err => {
+            console.log(err);
+        });
+
         let filePath = this.path.split('/embed')[1];
 
         // redirect to serve the image
